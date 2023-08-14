07ea01d83a1e73edf08b335f0a8d564f6e674327
amor44 <muigai349344@gmail.com>
2023-08-14 15:03:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+printf("      ____                   ____\n");
+printf("     /  /\\\\                 /  /\\\n");
+printf("    /  /:/_\\__             /  /::\\\n");
+printf("   /  /:/ /\\__\\           /  /:/\\:\\\n");
+printf("  /  /:/_/ / /\\:|  _____  /  /:/~/:/\n");
+printf(" /__/:/__\\/  \\:\\| /__/\\ /__/:/ /:/\n");
+printf(" \\  \\:\\ /\\__\\/:/ \\  \\:\\\\  \\:\\/:/\n");
+printf("  \\  \\:\\\\:\\__\\::/   \\__\\:\\ \\::/\n");
+printf("   \\  \\:\\\\/__/:/    /  /:/  \\/__/\n");
+printf("    \\  \\:\\__\\/:/    /__/:/\n");
+printf("     \\__\\/   \\:\\     \\__\\/\n");
+printf("               \\:\\\n");
+printf("                \\/\n");
+return (0);
+}
+
